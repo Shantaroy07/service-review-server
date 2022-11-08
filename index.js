5620acf06b8f897c126d554012d6e862b94e50bb
Shantaroy07 <shantaroy148@gmail.com>
2022-11-08 18:07:11 +0600

    home-service-get

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const cors = require('cors');
-
-
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onsb2w9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db('serviceReview').collection('services')
+
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 app.get('/', (req, res) => {
-    res.send(' server is running')
+    res.send('server is running')
 })
 
 app.listen(port, () => {
-    console.log('server running');
+    console.log(`Server running on ${port}`)
 })
+
